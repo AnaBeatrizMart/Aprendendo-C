2023f9e336b00d5a36f524ff7b277a57c5a3a366
Ana Beatriz Martins <36774340+AnaBeatrizMart@users.noreply.github.com>
2018-09-25 20:16:30 -0300

    Add files via upload

---
+++ define.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 10
+
+
+
+void main () {
+
+    // Imprimindo na tela
+    printf("%d", TAM);
+
+    int i;
+
+    // Contagem ate 10
+    for (i = 1; i <= TAM; i++){
+    printf("\n%d", i);
+    }
+
+
+    // Pausando o sistema
+    system ("pause");
+
+              }
